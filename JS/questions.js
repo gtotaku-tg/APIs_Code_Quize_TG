aa580c403fb35059dc0c057a574956427cdd0def
gtotaku-tg <124806960+gtotaku-tg@users.noreply.github.com>
2023-04-05 12:14:30 -0400

    Updated html

---
+++ JS/questions.js
@@ -0,0 +1,47 @@
+var quizQuestions = [
+    {
+      question: "1. What does API mean?",
+      answers: [
+        { text: "a. Application Program Interface", correct: true },
+        { text: "b. American Petroleum Institute", correct: false },
+        { text: "c. Accurate Position Indicator", correct: false },
+        { text: "d. Automated Pronunciation Instructor", correct: false }
+      ]
+    },
+    {
+      question: "What does CSS stand for?",
+      answers: [
+        { text: "Cascading Style Sheets", correct: true },
+        { text: "Computer Style Sheets", correct: false },
+        { text: "Colorful Style Sheets", correct: false },
+        { text: "Creative Style Sheets", correct: false }
+      ]
+    },
+    {
+      question: "What does JS stand for?",
+      answers: [
+        { text: "Java Source", correct: false },
+        { text: "Java Syntax", correct: false },
+        { text: "JavaScript", correct: true },
+        { text: "Joomla Script", correct: false }
+      ]
+    },
+    {
+        question: "What does DOM stand for?",
+        answers: [
+          { text: "a. Digital Ohmmeter", correct: false },
+          { text: "Dissolved Organic Matter", correct: false },
+          { text: "Document object model", correct: true },
+          { text: "Door of mentor", correct: false }
+        ]
+      },
+      {
+        question: "Which of the following statements about event listeners is true?",
+        answers: [
+          { text: "a. Event listeners are used only for handling mouse events.", correct: false },
+          { text: "b. Event listeners are added to elements using the `on-click` attribute.", correct: false },
+          { text: "c. Event listeners are triggered only by user interactions with a webpage.", correct: true },
+          { text: "d. Event listeners are not supported in modern web browsers.", correct: false }
+        ]
+      }
+  ];
