beb9e6181cfa862084071ef0cf8f0458223c868a
gtotaku-tg <124806960+gtotaku-tg@users.noreply.github.com>
2023-04-26 11:56:14 -0400

    update during develop

--- Assets/JS/questions.js
+++ Assets/JS/questions.js
@@ -1,88 +1,55 @@
 var quizQuestions = [
-    {
-      question: "1. What does API mean?",
-      answers: [
-        { text: "a. Application Program Interface", correct: true },
-        { text: "b. American Petroleum Institute", correct: false },
-        { text: "c. Accurate Position Indicator", correct: false },
-        { text: "d. Automated Pronunciation Instructor", correct: false }
-      ]
-    },
-    {
-      question: "What does CSS stand for?",
-      answers: [
-        { text: "Cascading Style Sheets", correct: true },
-        { text: "Computer Style Sheets", correct: false },
-        { text: "Colorful Style Sheets", correct: false },
-        { text: "Creative Style Sheets", correct: false }
-      ]
-    },
-    {
-      question: "What does JS stand for?",
-      answers: [
-        { text: "Java Source", correct: false },
-        { text: "Java Syntax", correct: false },
-        { text: "JavaScript", correct: true },
-        { text: "Joomla Script", correct: false }
-      ]
-    },
-    {
-        question: "What does DOM stand for?",
-        answers: [
-          { text: "a. Digital Ohmmeter", correct: false },
-          { text: "Dissolved Organic Matter", correct: false },
-          { text: "Document object model", correct: true },
-          { text: "Door of mentor", correct: false }
-        ]
-      },
-      {
-        question: "Which of the following statements about event listeners is true?",
-        answers: [
-          { text: "a. Event listeners are used only for handling mouse events.", correct: false },
-          { text: "b. Event listeners are added to elements using the `on-click` attribute.", correct: false },
-          { text: "c. Event listeners are triggered only by user interactions with a webpage.", correct: true },
-          { text: "d. Event listeners are not supported in modern web browsers.", correct: false }
-        ]
-      }
-  ];
-  // //set up quiz variables
-  // let currentQuestionIndex = 0;
-  // let score = 0;
-
-  // //get reference to the quiz element
-  // const quizContainer = document.getElementById('quiz-container');
-  // const questtionElement = document.getElementById('question');
-  // const answerButtonsElement = document.getElementById('answer-buttons');
-
-  // //add event listener to the start button
-  // document.getElementById('start-btn').addEventListener('click', startQuiz);
- 
-  // function startQuiz() {
-  //   console.log('started');
-  //   //start timer and display first question
-  //   //implement quiz logic here 
-  //   document.getElementById('start-btn').style.display = 'none';
-  //   //start timer and diaplay first question
-  //   const timerInterval = setInterval(function() {
-  //     //decrement time remaining
-  //     timeRemaing--;
-  //     //update timer display
-  //     document.getElementById('timer').textContent = "Time remaining: " + timeRemaing;
-  //     //check if time has run out
-  //     if (timeRemaing === 0) {
-  //       clearInterval(timerInterval);
-  //       endQuiz();
-  //     }
-  //   }, 1000);
-  //   //implement quiz logic
-  //   showQuestion();
-  // }
-
-  // function showQuestion() {
-  //   //display current question and answers
-  //   const currentQuestion = quizQuestions[currentQuestionIndex];
-  //   questtionElement.innerText = currentQuestion.question;
-  //   answerButtonsElement.innerHTML = '';
+  {
+    question: "What does API mean?",
+    answers: [
+      "Application Program Interface",
+      "American Petroleum Institute",
+      "Accurate Position Indicator",
+      "Automated Pronunciation Instructor",
+    ],
+    correctAnswer: "Application Program Interface",
+  },
+  {
+    question: "What does CSS stand for?",
+    answers: [
+      "Cascading Style Sheets",
+      "Computer Style Sheets",
+      "Colorful Style Sheets",
+      "Creative Style Sheets",
+    ],
+    correctAnswer: "Cascading Style Sheets",
+  },
+  {
+    question: "What does JS stand for?",
+    answers: [
+      "Java Source",
+      "Java Syntax",
+      "JavaScript",
+      "Joomla Script",
+    ],
+    correctAnswer: "JavaScript",
+  },
+  {
+    question: "What does DOM stand for?",
+    answers: [
+      "Digital Ohmmeter",
+      "Dissolved Organic Matter",
+      "Document object model",
+      "Door of mentor",
+    ],
+    correctAnswer: "Document object model",
+  },
+  {
+    question:
+      "Which of the following statements about event listeners is true?",
+    answers: [
+      "Event listeners are used only for handling mouse events.",
+      "Event listeners are added to elements using the `on-click` attribute.",
+      "Event listeners are triggered only by user interactions with a webpage.",
+      "Event listeners are not supported in modern web browsers.",
+    ],
+    correctAnswer: "Event listeners are triggered only by user interactions with a webpage.",
+  },
+];
 
 
-  // }
